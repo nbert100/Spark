3f03e3e599cd88cebc5acfef9edece66d8b9828a
nbert100 <nbert14@gmail.com>
2019-11-03 21:21:22 -0500

    show route error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,25 @@
 class SessionsController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path
+        else
+            #flash message
+            redirect_to '/login'
+            #only get errors if you render
+        end
     end
 
     def home
     end
 
     def destroy
+        session.clear
+        redirect_to '/'
     end
 end
