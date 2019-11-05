2166dc484399838e19bd9cba48fb5c8d58a862a2
nbert100 <nbert14@gmail.com>
2019-11-04 22:40:14 -0500

    add show page and index page for suitor

--- app/controllers/suitors_controller.rb
+++ app/controllers/suitors_controller.rb
@@ -1,8 +1,31 @@
 class SuitorsController < ApplicationController
 
     def new
+        @suitor = Suitor.new
     end
 
     def create
+        @suitor = Suitor.new(suitor_params)
+
+        if @suitor.save
+            redirect_to suitor_path(@suitor)
+        else
+            render :new
+        end
+    end
+
+    def index
+        @suitors = Suitor.all
+    end
+
+    def show
+        @suitor = Suitor.find_by(id: params[:id])
+    end
+
+    private
+
+    #for security purposes
+    def suitor_params
+        params.require(:suitor).permit(:name, :occupation, :age)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
   get '/users/:id', to: 'users#show', as: 'user'
   resources :users
-  resources :suitors, only:[:new, :create]
+  resources :suitors, only:[:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :suitors, only:[:show] is the same as get 'pets/:id' => 'pets#show'
 end
