0646691c0d150faa83289bcbbce5de9095a4135f
nbert100 <nbert14@gmail.com>
2019-11-06 19:02:25 -0500

    add error partial and error message to login with flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id 
             redirect_to user_path(@user)
         else
-            #flash message
+            flash[:error] = "Invalid Login. Please try again."
             redirect_to '/login'
             #only get errors if you render
         end
